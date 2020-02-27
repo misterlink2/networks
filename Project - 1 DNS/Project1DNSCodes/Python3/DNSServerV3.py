688893d629bb3c4de4817ee80960b80d011158e2
misterlink2 <austinbasala@ymail.com>
2020-02-27 11:11:06 -0600

    started p1

--- Project - 1 DNS/Project1DNSCodes/Python3/DNSServerV3.py
+++ Project - 1 DNS/Project1DNSCodes/Python3/DNSServerV3.py
@@ -11,16 +11,18 @@ def main():
 	port = 5001 # Port number.
 
 	#create a socket object, SOCK_STREAM for TCP
-	socket = socket(AF_INET, SOCK_STREAM)
-
+	serversocket = socket(AF_INET, SOCK_STREAM)
+	print("socket created")
 
 	#bind socket to the current address on port 5001
-	socket.bind(host, 80)
+	serversocket.bind((host, port))
 
+	print("socket bound")
 
 	#Listen on the given socket maximum number of connections queued is 20
 	serversocket.listen(20)
 
+	print("socket listening")
 
 	monitor = threading.Thread(target=monitorQuit, args=[])
 	monitor.start()
@@ -29,7 +31,7 @@ def main():
 
 	while 1:
 		#blocked until a remote machine connects to the local port 5001
-		connectionSock, addr = sSock.accept()
+		connectionSock, addr = serversocket.accept()
 		server = threading.Thread(target=dnsQuery, args=[connectionSock, addr[0]])
 		server.start()
 
@@ -37,20 +39,19 @@ def dnsQuery(connectionSock, srcAddress):
 	#check the DNS_mapping.txt to see if the host name exists
 	#set local file cache to predetermined file.
     #create file if it doesn't exist
+			# file.close()
+		#if it does exist, read the file line by line to look for a
+		#match with the query sent from the client
+		#If match, use the entry in cache.
+	    #However, we may get multiple IP addresses in cache, so call dnsSelection to select one.
+
 	hostname
 	ipadress
 	with open("DNS_mapping.txt","r") as file:
 		if srcAddress in file.read():
 			hostname = srcAddress
-		# file.close()
-	#if it does exist, read the file line by line to look for a
-	#match with the query sent from the client
-	#If match, use the entry in cache.
-
-    #However, we may get multiple IP addresses in cache, so call dnsSelection to select one.
-	dnsSelection(srcAddress)
-
-		else
+			dnsSelection(srcAddress)
+		else:
 			#If no lines match, query the local machine DNS lookup to get the IP resolution
 			hostname = gethostbyname(srcAddress)
 			with open("DNS_mapping.txt","a+") as file:
@@ -60,7 +61,7 @@ def dnsQuery(connectionSock, srcAddress):
 				# file.close()
 
 	#print response to the terminal
-	print("hostname is: %s"hostname)
+	print("hostname is: %s", hostname)
 
 	#send the response back to the client
 	connectionSock.sendall(hostname)
